ded90da0db649d4563f2fba63321a88c117d8ffe
mihasket <miha.sket@gmail.com>
2024-04-21 20:24:06 +0200

    feat: chip8 basic struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct Chip8 {
+    opcode: u16,
+    memory: [u8; 4096],
+    cpu_register_v: [u8; 15],
+    register_index: u16,
+    pc: u16,
+    gfx: [u8; 64 * 32],
+    delay_timer: u8,
+    sound_timer: u8,
+    // Stack includes stack pointer
+    stack: Vec<u16>,
+    key: [u8; 16],
+}
+
 fn main() {
     println!("Hello, world!");
 }
