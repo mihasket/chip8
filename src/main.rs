99a29b59bc7918a1dff3837a14224a9b97d6266b
mihasket <miha.sket@gmail.com>
2024-04-22 22:39:16 +0200

    fix: check for cmd arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::env;
 
 mod chip_8;
@@ -5,6 +6,10 @@ use chip_8::Chip8;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("No command line argument present");
+    }
 
     let mut chip8 = Chip8::initialize();
     chip8.load_fontset();
